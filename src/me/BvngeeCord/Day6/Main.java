172cff674b43e09edeeea1bd9749f79c76ded221
Bvngee Cord <nystromjp@gmail.com>
2021-12-09 09:59:10 -0800

    Finish day 6 part 2

--- src/me/BvngeeCord/Day6/Main.java
+++ src/me/BvngeeCord/Day6/Main.java
@@ -1,8 +1,6 @@
 package me.BvngeeCord.Day6;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import me.BvngeeCord.Util;
 
@@ -11,26 +9,31 @@ public class Main {
     public static void main (String[] args){
 
         int[] fish = Arrays.stream(Util.getPuzzleInput(6).get(0).split(",")).mapToInt(Integer::parseInt).toArray();
+
         long[] score = new long[9];
         for (int n : fish){
             score[n] ++;
         }
 
         System.out.println("part one " + calculateFish(score, 80));
-        System.out.println("part two " + calculateFish(score, 256));
 
-    }
-
-    public static int calculateFish(long[] score, int days){
-
-        for (int n=0; n<days; n++){
-            System.arraycopy(score, 1, score, 0, score.length-1);
-
-            score[6] += score[0];
-            score[8] = score[0];
+        score = new long[9];
+        for (int n : fish){
+            score[n] ++;
         }
 
-        return (int) Arrays.stream(score).sum();
+        System.out.println("part two " + calculateFish(score, 256));
+
+        System.out.println(score[0]);
+    }
+
+    public static long calculateFish(long[] score, int days){
+
+        for (int n=0; n<days; n++){
+            score = new long[]{score[1], score[2], score[3], score[4], score[5], score[6], score[7]+ score[0], score[8], score[0]};
+        }
+
+        return Arrays.stream(score).sum();
     }
 
 }
