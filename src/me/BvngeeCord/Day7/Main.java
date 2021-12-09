d9451d248a7c251f9ce082008a7cb15ad4f4429a
Bvngee Cord <nystromjp@gmail.com>
2021-12-09 10:04:40 -0800

    Day 7 begun

---
+++ src/me/BvngeeCord/Day7/Main.java
@@ -0,0 +1,13 @@
+package me.BvngeeCord.Day7;
+
+import me.BvngeeCord.Util;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int[] input = Util.getPuzzleInput(7).stream().mapToInt(Integer::parseInt).toArray();
+
+    }
+
+}
