9327d9972ad48f0541fcf570cd1cfed072bb827e
Bvngee Cord <nystromjp@gmail.com>
2021-12-13 09:20:57 -0800

    Day 7 begun 2.1

--- src/me/BvngeeCord/Day7/Main.java
+++ src/me/BvngeeCord/Day7/Main.java
@@ -12,8 +12,9 @@ public class Main {
 
         int sum = 0;
         for (int i = 0; i < input.length; i++) {
-
+            sum += input[i];
         }
+        System.out.println(sum/input.length);
 
     }
 
