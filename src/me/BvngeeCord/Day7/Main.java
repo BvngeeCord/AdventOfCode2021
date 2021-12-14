d47cc8748d478bf09e7b82538327530f550abf90
Bvngee Cord <nystromjp@gmail.com>
2021-12-14 10:30:19 -0800

    Day 7, on part 2

--- src/me/BvngeeCord/Day7/Main.java
+++ src/me/BvngeeCord/Day7/Main.java
@@ -10,12 +10,36 @@ public class Main {
 
         int[] input = Arrays.stream(Util.getPuzzleInput(7).get(0).split(",")).mapToInt(Integer::parseInt).toArray();
 
-        int sum = 0;
-        for (int i = 0; i < input.length; i++) {
-            sum += input[i];
-        }
-        System.out.println(sum/input.length);
+        System.out.println("Part one: " + partOne(input, Arrays.stream(input).max().getAsInt()));
+        System.out.println("Part two: " + partTwo(input, Arrays.stream(input).max().getAsInt()));
 
     }
 
-}
+    public static int partOne(int[] input, int max){
+        int bestPos = Integer.MAX_VALUE;
+        for (int i = 0; i < max; i++){
+            int fuelUsed = 0;
+            for (int j : input) {
+                fuelUsed += Math.abs(j - i);
+            }
+            System.out.println(fuelUsed);
+            if (fuelUsed < bestPos) bestPos = fuelUsed;
+        }
+        return bestPos;
+    }
+
+    public static int partTwo(int[] input, int max) {
+        int bestPos = Integer.MAX_VALUE;
+        for (int i = 0; i < max; i++) {
+            int fuelUsed = 0;
+            for (int j : input) {
+                fuelUsed += (Math.abs(i - j) * Math.abs(i - j)+1) / 2;
+                System.out.println(i + " " + j + " " + (Math.abs(i - j) * Math.abs(i - j)+1) / 2);
+            }
+            System.out.println(fuelUsed);
+            if (fuelUsed < bestPos) bestPos = fuelUsed;
+        }
+        return bestPos;
+    }
+
+    }
